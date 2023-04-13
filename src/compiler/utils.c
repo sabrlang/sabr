1bc13ced98069f3e037bab5be6de6adf2af98b54
Coma <bmh4080@naver.com>
2023-04-14 01:46:31 +0900

    Implemented utils new string slice

--- src/compiler/utils.c
+++ src/compiler/utils.c
@@ -3,8 +3,6 @@
 char* new_string_slice(const char* source, size_t begin_index, size_t end_index) {
 	size_t size = end_index - begin_index + 1;
 
-	printf("size = %zu\n", size);
-
 	char* new_string = (char*) malloc(size * sizeof(char));
 	if (!new_string) return NULL;
 
