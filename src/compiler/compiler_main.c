f71feec0e4e4c8c686229e314d8d31c0502f6b78
Coma <bmh4080@naver.com>
2023-08-12 22:58:40 +0900

    .

--- src/compiler/compiler_main.c
+++ src/compiler/compiler_main.c
@@ -9,14 +9,16 @@ int main() {
 
 	bytecode* bc = sabr_compiler_compile_file(&comp, "test.sabrc");
 
+	if (!bc) return 1;
+	
 	print_bytecode(bc);
 
 	if (!sabr_compiler_save_bytecode(&comp, bc, "test.sabre")) {
-		return 1;
+		return 2;
 	}
 
 	sabr_free_bytecode(bc);
 	free(bc);
 
-	if (!sabr_compiler_del(&comp)) return 2;
+	if (!sabr_compiler_del(&comp)) return 3;
 }
